a4e8119fbbcc8eeef1cd7437adf5e4cd28af01f1
Qasim Laghari <go.jets.laghari@gmail.com>
2016-04-17 12:14:00 -0500

    Add route for creating new form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
-export default () => {
-  return <div> List of blog posts </div>;
+class PostsIndex extends Component {
+
+  // Life Cycle function for first time render
+  componentWillMount() {
+      console.log('This would be a good time to call an action creator');
+      this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+      <div className = "text-xs-right">
+        <Link to="/posts/new" className="btn btn-primary">
+          Add a new post
+        </Link>
+      </div>
+      List of blog posts
+      </div>
+    );
+  }
 }
+
+//Elimininate due to refactor
+
+//function mapDispatchToProps(dispatch) {
+//  return bindActionCreators({fetchPosts}, dispatch);
+//}
+
+export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);

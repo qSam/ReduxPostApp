f404c6ead9e3ff3507cafbabfcdeef92d15c77b2
Qasim Laghari <go.jets.laghari@gmail.com>
2016-04-18 23:28:31 -0500

    Add post details

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,8 +8,25 @@ class PostsShow extends Component {
   }
 
   render() {
-    return <div>Show post {this.props.params.id} </div>
+    const {post} = this.props;
+
+    //console.log(this.props.post);
+    if(!this.props.post){
+        return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+  );
   }
+}
+
+function mapStateToProps(state) {
+  return {post: state.posts.post}
 }
 
 export default connect (null, {fetchPost})(PostsShow);
